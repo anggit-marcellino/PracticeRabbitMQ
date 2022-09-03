b6006a9116f090703469331b410fa03a481eea83
anggit-marcellino <anggit.marcellino@iseller.id>
2022-09-04 01:21:10 +0700

    product

---
+++ PracticeRabbitMQ/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using PracticeRabbitMQ.Models;
+
+namespace PracticeRabbitMQ.Services
+{
+    public interface IProductService
+    {
+        public IEnumerable<Product> GetProducts();
+        public Product GetProductById(int id);
+        public Product AddProduct(Product product);
+        public Product UpdateProduct(Product product);
+        public bool DeleteProduct(int id);
+    }
+}
